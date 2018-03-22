b9bfad39b6bc93360fa1863eee62bf4a1f6f6d84
Christian Paling <christian.paling@googlemail.com>
2018-03-22 13:18:40 +0100

    Implement main parse method

--- lib/mrz.rb
+++ lib/mrz.rb
@@ -8,5 +8,28 @@ require "mrz/td2_parser"
 require "mrz/td3_parser"
 
 module MRZ
-  # Your code goes here...
+  def self.parse(mrz_code)
+    case determine_type(mrz_code)
+    when :td1
+      TD1Parser.new(mrz_code).parse
+    when :td2
+      TD2Parser.new(mrz_code).parse
+    when :td3
+      TD3Parser.new(mrz_code).parse
+    end
+  end
+
+  private
+
+    def self.determine_type(code)
+      if code.size == 3
+        :td1
+      elsif code.size == 2 && code.first.size == 36
+        :td2
+      elsif code.size == 2 && code.first.size == 44
+        :td3
+      else
+        raise MRZ::InvalidFormatError, "invalid or unsupported mrz code given"
+      end
+    end
 end
--- spec/mrz_spec.rb
+++ spec/mrz_spec.rb
@@ -2,4 +2,38 @@ RSpec.describe MRZ do
   it "has a version number" do
     expect(MRZ::VERSION).not_to be nil
   end
+
+  describe "#parse" do
+    it "should be able to correctly parse a td1 ID card" do
+      id_card = [
+        "IDD<<T220001293<<<<<<<<<<<<<<<",
+        "6408125<2010315D<<<<<<<<<<<<<4",
+        "MUSTERMANN<<ERIKA<PAULA<ANNA<<"
+      ]
+
+      expect(MRZ.parse(id_card).valid?).to eq(true)
+    end
+
+    it "should be able to correctly parse a td2 ID card" do
+      id_card = [
+        "I<UTOERIKSSON<<ANNA<MARIA<<<<<<<<<<<",
+        "D231458907UTO7408122F1204159<<<<<<<6"
+      ]
+
+      expect(MRZ.parse(id_card).valid?).to eq(true)
+    end
+
+    it "should be able to correctly parse a td3 passport" do
+      passport = [
+        "P<D<<ADENAUER<<KONRAD<HERMANN<JOSEPH<<<<<<<<",
+        "1234567897D<<7601059M6704115<<<<<<<<<<<<<<<2"
+      ]
+
+      expect(MRZ.parse(passport).valid?).to eq(true)
+    end
+
+    it "should raise an error if it could not determine type" do
+      expect { MRZ.parse(["hi"]) }.to raise_error(MRZ::InvalidFormatError)
+    end
+  end
 end
