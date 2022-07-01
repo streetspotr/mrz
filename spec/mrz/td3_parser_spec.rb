e05022ef7e6dd3fd89b1cab4413cbc9008171065
Ianaré Sévi <ianare@mindee.co>
2022-07-01 15:40:11 +0200

    add unit tests

--- spec/mrz/td3_parser_spec.rb
+++ spec/mrz/td3_parser_spec.rb
@@ -1,9 +1,42 @@
 RSpec.describe MRZ::TD3Parser do
-  let(:mrz_array) do
+  let(:mrz_array) do [
     [
       "P<D<<ADENAUER<<KONRAD<HERMANN<JOSEPH<<<<<<<<",
       "1234567897D<<7601059M6704115<<<<<<<<<<<<<<<2"
-    ]
+    ],
+    [
+      "P<UTOERIKSSON<<ANNA<MARIA<<<<<<<<<<<<<<<<<<<",
+      "1234567897D<<7601059M6704115<<<<<<<<<<<<<<<2"
+    ],
+    [
+      "P<UTOHENG<<DEBORAH<MING<LO<<<<<<<<<<<<<<<<<<",
+      "1234567897D<<7601059M6704115<<<<<<<<<<<<<<<2"
+    ],
+    [
+      "P<UTOOCONNOR<<ENYA<SIOBHAN<<<<<<<<<<<<<<<<<<",
+      "1234567897D<<7601059M6704115<<<<<<<<<<<<<<<2"
+    ],
+    [
+      "P<UTOVAN<DER<MUELLEN<<MARTIN<<<<<<<<<<<<<<<<",
+      "1234567897D<<7601059M6704115<<<<<<<<<<<<<<<2"
+    ],
+    [
+      "P<UTOARKFREITH<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+      "1234567897D<<7601059M6704115<<<<<<<<<<<<<<<2"
+    ],
+    [
+      "P<UTOSMITH<JONES<<SUSIE<MARGARET<<<<<<<<<<<<",
+      "1234567897D<<7601059M6704115<<<<<<<<<<<<<<<2"
+    ],
+    [
+      "P<UTOAL<BASRI<<HUDA<MUHAMMAD<JAWAD<<<<<<<<<<",
+      "1234567897D<<7601059M6704115<<<<<<<<<<<<<<<2"
+    ],
+    [
+      "P<UTOVILARCHAO<FERNANDEZ<<JOSE<RAMON<<<<<<<<",
+      "1234567897D<<7601059M6704115<<<<<<<<<<<<<<<2"
+    ],
+  ]
   end
 
   describe "#initialize" do
@@ -13,16 +46,22 @@ RSpec.describe MRZ::TD3Parser do
   end
 
   describe "#parse" do
+    it "should accept all valid formats" do
+      mrz_array.each do |ary |
+        MRZ::TD3Parser.new(ary).parse
+      end
+    end
+
     it "should raise an error if array contains more than two strings" do
-      expect { MRZ::TD3Parser.new(mrz_array.concat(["hello!!"])).parse }.to raise_error(MRZ::InvalidFormatError)
+      expect { MRZ::TD3Parser.new(["one", "two", "three"]).parse }.to raise_error(MRZ::InvalidFormatError)
     end
 
     it "should raise an error if array contains less than two strings" do
-      expect { MRZ::TD3Parser.new([ mrz_array[0] ]).parse }.to raise_error(MRZ::InvalidFormatError)
+      expect { MRZ::TD3Parser.new(["one", ]).parse }.to raise_error(MRZ::InvalidFormatError)
     end
 
     it "should return a correct MRZ::Result instance" do
-      res = MRZ::TD3Parser.new(mrz_array).parse
+      res = MRZ::TD3Parser.new(mrz_array[0]).parse
 
       expect(res.birth_date).to                  eq(Date.new(1976, 1, 5))
       expect(res.birth_date_check_digit).to      eq("9")
@@ -44,8 +83,8 @@ RSpec.describe MRZ::TD3Parser do
 
     it "should raise an error if line one does not match the required format" do
       ary = [
-        mrz_array[0][0..-2],
-        mrz_array[1]
+        mrz_array[0][0][0..-2],
+        mrz_array[0][1]
       ]
 
       expect { MRZ::TD3Parser.new(ary).parse }.to raise_error(MRZ::InvalidFormatError)
@@ -53,8 +92,8 @@ RSpec.describe MRZ::TD3Parser do
 
     it "should raise an error if line two does not match the required format" do
       ary = [
-        mrz_array[0],
-        mrz_array[1][0..-2]
+        mrz_array[0][0],
+        mrz_array[0][1][0..-2]
       ]
 
       expect { MRZ::TD3Parser.new(ary).parse }.to raise_error(MRZ::InvalidFormatError)
